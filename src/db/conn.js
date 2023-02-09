db235e2296a2a1a1cf62885b41b16e48b3db085b
Kartik Bhardwaj <developer.kartikbhardwaj@gmail.com>
2023-02-09 17:29:56 +0530

    ""

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/Bookish');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("mongoose is connected");
 
 }
